f75de5d23adcc997c7f4f04ddf038478f08cee55
Deeksha-coder-debug <deeksha.jeypore@gmail.com>
2024-12-31 20:05:48 +0530

    Create 3 Largest element.py
    Problem statement : 
    
    Largest element in an Array
    
    Given an array arr. The task is to find the largest element in the given array. 
    
    Examples: 
    
    Input: arr[] = [10, 20, 4]
    Output: 20
    Explanation: Among 10, 20 and 4, 20 is the largest. 
    
    
    Input: arr[] = [20, 10, 20, 4, 100]
    Output: 100

---
+++ 3 Largest element.py
@@ -0,0 +1,13 @@
+#Iterative approach
+
+class Solution:
+    def largest(self, arr):
+        # code here
+        l=arr[0]
+        for i in arr:
+            if i>l:
+                l=i
+        return l
+
+#Recursive approach
+
